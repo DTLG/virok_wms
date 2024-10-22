7b15de03d3416dda48894132e5ab1e28b0b4bbd6
aa <aa>
2024-10-22 17:34:41 +0300

    new commit

---
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/andriysalabay/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/andriysalabay/Desktop/11111/virok_wms"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=2.5.5"
+export "FLUTTER_BUILD_NUMBER=2.5.5"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/andriysalabay/Desktop/11111/virok_wms/.dart_tool/package_config.json"

b85b5ee54fba31b6a00ecb4b4c7fee3eced46e4b
Emm Nov <emmnauil.novakovych@gmail.com>
2024-10-23 17:57:59 +0300

    Ios new update

--- lib/ui/widgets/camera_scaner_button.dart
+++ lib/ui/widgets/camera_scaner_button.dart
@@ -9,17 +9,28 @@ class CameraScanerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: ((ctx) => AiBarcodeScanner(
-                        controller: MobileScannerController(
-                          detectionTimeoutMs: 1000,
-                          detectionSpeed: DetectionSpeed.normal,
-                        ),
-                      ))));
-        },
-        icon: const Icon(Icons.qr_code_scanner_rounded));
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (ctx) => AiBarcodeScanner(
+              onDetect: (p0) {
+                String barcode = p0.barcodes.first.rawValue ?? '';
+                if (barcode != ''){
+                scan(barcode);
+                Navigator.pop(ctx);
+
+                }
+              },
+              controller: MobileScannerController(
+                detectionTimeoutMs: 1000,
+                detectionSpeed: DetectionSpeed.normal,
+              ),
+            ),
+          ),
+        );
+      },
+      icon: const Icon(Icons.qr_code_scanner_rounded),
+    );
   }
 }
